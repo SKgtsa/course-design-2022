11639bad71a731b51f6b54e0f3a9ae09a5efee08
Idonotkownwho <109019984+Idonotkownwho@users.noreply.github.com>
2022-11-07 16:12:23 +0800

    Create AvatarUploadTestView.vue

---
+++ front/src/components/AvatarUploadTestView.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-upload action="#" list-type="picture-card" :auto-upload="false">
+    <el-icon><Plus /></el-icon>
+
+    <template #file="{ file }">
+      <div>
+        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+        <span class="el-upload-list__item-actions">
+          <span
+            class="el-upload-list__item-preview"
+            @click="handlePictureCardPreview(file)"
+          >
+            <el-icon><zoom-in /></el-icon>
+          </span>
+          <span
+            v-if="!disabled"
+            class="el-upload-list__item-delete"
+            @click="handleDownload(file)"
+          >
+            <el-icon><Download /></el-icon>
+          </span>
+          <span
+            v-if="!disabled"
+            class="el-upload-list__item-delete"
+            @click="handleRemove(file)"
+          >
+            <el-icon><Delete /></el-icon>
+          </span>
+        </span>
+      </div>
+    </template>
+  </el-upload>
+
+  <el-dialog v-model="dialogVisible">
+    <img w-full :src="dialogImageUrl" alt="Preview Image" />
+  </el-dialog>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue'
+
+import type { UploadFile } from 'element-plus'
+import service from "@/request";
+
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+const disabled = ref(false)
+
+const handleRemove = (file: UploadFile) => {
+  console.log(file)
+  service.post('/upload/change_avatar',{token: localStorage.getItem('token'), file: file}).then(response => {
+    const data = response.data;
+    console.log(response)
+    if(data.success){
+      console.log('success')
+    }
+  })
+}
+
+const handlePictureCardPreview = (file: UploadFile) => {
+  dialogImageUrl.value = file.url!
+  dialogVisible.value = true
+}
+
+const handleDownload = (file: UploadFile) => {
+  console.log(file)
+  service.post('/upload/change_avatar',{token: localStorage.getItem('token'), file: file}).then(response => {
+    const data = response.data;
+    console.log(response)
+    if(data.success){
+      console.log('success')
+    }
+  })
+}
+</script>
